0444e73c62108a8dafa61402fd2cd387d5f19507
Dammmer <dochky.1@mail.ru>
2025-06-27 11:31:45 +0500

    update port

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import lessonRoutes from './routes/lesson';
 dotenv.config();
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({
   origin: '*',
