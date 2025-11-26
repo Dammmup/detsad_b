55fae823f5c8944d1528ea909dd94d936b4413f6
Дамир Биянхо <damir.-@mail.ru>
2025-11-26 21:27:00 +0500

    returned

--- src/app.ts
+++ src/app.ts
@@ -48,14 +48,12 @@ const app = express();
 
 
 // ✅ Добавляем CORS до всех роутов
-const corsOptions = {
+app.use(cors({
   origin: process.env.CORS_ORIGIN || '*', // используем переменную окружения или все источники
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: false, // т.к. cookies ты отключил
-};
-
-app.use(cors(corsOptions));
+}));
 
 // app.use(cookieParser()); // ❌ полностью убираем middleware для парсинга cookies
 app.use(express.json({ limit: '10mb' }));
